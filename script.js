e07959e06c3d1edc4b84188d74c229c597f69764
Leandro Sena <leo.sena777@gmail.com>
2019-11-20 16:28:16 -0300

    personagem principal + mecanica de 3 pulo + fisica

--- script.js
+++ script.js
@@ -6,6 +6,50 @@ var  ctx;
 var  ALTURA = window.innerHeight;
 var  LARGURA = window.innerWidth;
 var  frames=0;
+var maxPulos = 3;
+
+// objetos
+
+var chao = {
+    y:550,
+    altura:50,
+    cor:"#ffdf70",
+    desenhar: function(){
+        ctx.fillStyle = this.cor;
+        ctx.fillRect(0,this.y, LARGURA, this.altura);
+    }
+};
+
+var bloco = {
+    x:50,
+    y:0,
+    altura:50,
+    largura:50,
+    cor:"#ff4e4e",
+    gravidade: 1.5,
+    velocidade: 0,
+    forcaDoPulo:20,
+    qntPulos:0,
+    atualizar: function(){
+        this.velocidade+= this.gravidade;
+        this.y += this.velocidade;
+        
+        if(this.y > chao.y - this.altura){
+            this.y = chao.y - this.altura;
+            this.qntPulos = 0;
+        }
+    },
+    pula:function(){
+        if(this.qntPulos < maxPulos){
+            this.velocidade = -this.forcaDoPulo;
+            this.qntPulos++;
+        }
+    },
+    desenhar: function(){
+        ctx.fillStyle = this.cor;
+        ctx.fillRect(this.x,this.y,this.largura,this.altura);
+    }
+}
 
 
 function main(){
@@ -27,7 +71,7 @@ function main(){
 }
 
 function clique(event){
-    alert("click")
+    bloco.pula();
 }
 
 function roda(){
@@ -41,11 +85,15 @@ function roda(){
 
 function atualizar(){
     frames++;
+    bloco.atualizar();
 }
 
 function desenhar(){
     ctx.fillStyle = "#50beff";
     ctx.fillRect(0,0,LARGURA,ALTURA);
+
+    chao.desenhar();
+    bloco.desenhar();
 }
 
 main(); 
